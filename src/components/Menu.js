2abd7e88f72f9e6f3f9157c482f4429fe0d8da4a
jonas-andrade <jonas6116m@outlook.com>
2024-09-24 21:58:06 -0300

    components iniciais - searchingfilmes

---
+++ src/components/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Menu = ()=>{
+    return(
+        <ul className="menu">
+        <li><a href="#"><span><i class="fas fa-film movie-icon" /></span></a></li>
+        <li><a href="#"><span>Filmes</span></a></li>
+        <li><a href="#"><span>Séries</span></a></li>
+        </ul>
+    )
+}
+export default Menu;
+
