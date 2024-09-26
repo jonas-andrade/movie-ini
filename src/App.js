95dec7ef24eaa1de31a4675b9b0675e8baa47124
jonas-andrade <jonas6116m@outlook.com>
2024-09-26 00:04:17 -0300

    App.js Refactoring pronto. - Revisão de Código

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
-import React, { useEffect, useState } from 'react';
-// import './styles.css'; 
+import React from 'react';
+import './styles.css'; 
 import Menu from './components/Menu';
 import Movie from './components/Movie';
 import { movieApi } from './service/api';
 
 const App = ()=> {
-
-    const [movies, setMovies] = useState([]);
-    useEffect(() => {
-        // Chama a função que busca os dados da API
-        const getMovies = async () => {
-            const data = await movieApi.fetchMoviesData();
-            if (data && data.movies) {
-                setMovies(data.movies); // Define os filmes no estado
-            }
-        };
-        getMovies();
-    }, []);
-
-
     return (
        
            <div className='container'>
-            {/* <Menu />
+            <Menu />
             <h1>Tecnologia, Sci-Fi e Tendências do Futuro</h1>
 
-            <Movie/> */}
-              <ul>
-                {movies.map(movie => (
-                    <li key={movie.id}>
-                        {movie.title} {/* Exibe o título do filme */}
-                    </li>
-                ))}
-            </ul>       
+              <h2>Ação</h2>
+              <Movie/>
+            
+                
  
            </div>
         
